61b99c03f32e57d7e72ee3b4d38c24a125edbab5
Thomas Barron <barron.tom@gmail.com>
2020-04-22 16:57:49 -0400

    updated Customer and Controller.

--- GC_Lab_15.3/Controllers/CustomersController.cs
+++ GC_Lab_15.3/Controllers/CustomersController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using GC_Lab_15._3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace GC_Lab_15._3.Controllers
 {
@@ -11,5 +15,47 @@ namespace GC_Lab_15._3.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        private readonly string connectionString;
+
+        public CustomersController(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("default");
+        }
+        [HttpGet("all")]
+        public object Get()
+        {
+            IEnumerable<Customer> customers = new Customer[0];
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string queryString = "SELECT  CustomerID, ContactName, ContactTitle, ";
+                queryString += "Country ";
+                queryString += "FROM Customers ";
+                queryString += "ORDER BY CustomerID ";
+
+                customers = conn.Query<Customer>(queryString);
+            }
+
+            return customers;
+        }
+        [HttpGet("country/{country}")]
+        public object Get(string country = null)
+        {
+            IEnumerable<Customer> customers = new Customer[0];
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string queryString = "SELECT  CustomerID, ContactName, ContactTitle, ";
+                queryString += "Country ";
+                queryString += "FROM Customers ";
+                queryString += "Where Country = @Country ";
+                queryString += "ORDER BY CustomerID ";
+                
+
+                customers = conn.Query<Customer>(queryString, new { Country = country });
+            }
+
+            return customers;
+        }
     }
 }
---
+++ GC_Lab_15.3/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GC_Lab_15._3.Models
+{
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Country { get; set; }
+    }
+}
