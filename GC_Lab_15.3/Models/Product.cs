f503dd1ddcfd6ee6c569b1ec7284061e30aea738
Laura Brock <laurabrock@quickenloans.com>
2020-04-22 16:59:28 -0400

    added product controller and model

---
+++ GC_Lab_15.3/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_Lab_15._3.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
